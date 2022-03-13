664ffe4350f328e768c7e68b1ca8b713f03ed333
Julia Branke <Julia.Branke@gmx.co.uk>
2022-03-13 08:07:23 +0000

    Scaffolding for user interaction

---
+++ src/main/scala/marsRover/marsRoverMain.scala
@@ -0,0 +1,16 @@
+package marsRover
+
+object marsRoverMain
+{
+  def main(args: Array[String]): Unit = {
+    println("Please enter Grid values for x and y (e.g. 5 5)")
+
+    println("Please enter Coordinates for first rover (e.g. 1 2 N)")
+
+    println("Please enter moving pattern for first rover (e.g. LMLMLMLMM)")
+
+    println("Please enter Coordinates for second rover (e.g. 3 3 E)")
+
+    println("Please enter moving pattern for second rover (e.g. MMRMMRMRRM)")
+  }
+}
