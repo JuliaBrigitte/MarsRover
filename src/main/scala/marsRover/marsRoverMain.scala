46394ecb767e3957051e21650394aea9bb579e80
Julia Branke <Julia.Branke@gmx.co.uk>
2022-03-13 13:11:13 +0000

    Check input for errors with regex

--- src/main/scala/marsRover/marsRoverMain.scala
+++ src/main/scala/marsRover/marsRoverMain.scala
@@ -3,36 +3,52 @@ package marsRover
 object marsRoverMain
 {
   def main(args: Array[String]): Unit = {
-    println("Please enter Grid values for x and y (e.g. 5 5)")
-    var input=scala.io.StdIn.readLine()
-//    val Array(xGrid, yGrid) = input.split(" ")map(_.toInt)
-//    println("xGrid "+ xGrid)
-//    println("yGrid "+ yGrid)
+    var input=""
+    while (!input.matches("[0-9]* [0-9]*"))
+    {
+      println("Please enter Grid values for x and y (e.g. 5 5)")
+      input=scala.io.StdIn.readLine()
+    }
+    val Array(xGrid, yGrid) = input.split(" ").map(_.toInt)
+    println("xGrid "+ xGrid)
+    println("yGrid "+ yGrid)
 
-    println("Please enter Coordinates for first rover (e.g. 1 2 N)")
-    var inputRover1=scala.io.StdIn.readLine()
+    var inputRover1=""
+    while (!inputRover1.matches("[0-9]* [0-9]* [NSEW]+"))
+    {
+      println("Please enter Coordinates for first rover (e.g. 1 2 N)")
+      inputRover1=scala.io.StdIn.readLine()
+    }
     val Array(coordX1, coordY1, direction1)=inputRover1.split(" ")
 //    println("coordX1 "+ coordX1)
 //    println("coordY1 "+ coordY1)
 //    println("direction1 "+ direction1)
-
-    println("Please enter moving pattern for first rover (e.g. LMLMLMLMM)")
-    var roverMovement1=scala.io.StdIn.readLine()
+    var roverMovement1=""
+    while (!roverMovement1.matches("[LRM]+"))
+    {
+      println("Please enter moving pattern for first rover (e.g. LMLMLMLMM)")
+      roverMovement1=scala.io.StdIn.readLine()
+    }
 //    println("roverMovement1 "+ roverMovement1)
 
-    println("Please enter Coordinates for second rover (e.g. 3 3 E)")
-    var inputRover2=scala.io.StdIn.readLine()
+    var inputRover2=""
+    while (!inputRover2.matches("[0-9]* [0-9]* [NSEW]+"))
+    {
+      println("Please enter Coordinates for second rover (e.g. 3 3 E)")
+      inputRover2=scala.io.StdIn.readLine()
+    }
     val Array(coordX2, coordY2, direction2)=inputRover2.split(" ")
 //    println("coordX2 "+ coordX2)
 //    println("coordY2 "+ coordY2)
 //    println("direction2 "+ direction2)
 
-    println("Please enter moving pattern for second rover (e.g. MMRMMRMRRM)")
-    var roverMovement2=scala.io.StdIn.readLine()
-/*
-    println("roverMovement2 "+ roverMovement2)
-*/
-
+    var roverMovement2=""
+    while (!roverMovement2.matches("[RLM]+"))
+    {
+      println("Please enter moving pattern for second rover (e.g. MMRMMRMRRM)")
+      roverMovement2=scala.io.StdIn.readLine()
+    }
+//    println("roverMovement2 "+ roverMovement2)
     println("---------------------------------")
     println("Result:")
 
@@ -41,18 +57,11 @@ object marsRoverMain
     var marsRover1=new MarsRover(coordinatesMarsRover1,directionMarsRover1)
     marsRover1.moveTo(roverMovement1)
     marsRover1.printCurrentStatus()
-    
+
     var coordinatesMarsRover2=new Coordinates(coordX2.toInt,coordY2.toInt)
     var directionMarsRover2=new Direction(direction2.charAt(0))
     var marsRover2=new MarsRover(coordinatesMarsRover2,directionMarsRover2)
     marsRover2.moveTo(roverMovement2)
     marsRover2.printCurrentStatus()
-
-
-
-
-
-
-
   }
 }
